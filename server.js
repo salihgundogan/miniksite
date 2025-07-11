a166b1410a86ad9dee29f992e08c5a5e5e3991fd
salihgundogan <salih.gundogan@istun.edu.tr>
2025-07-11 10:46:57 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Public klasörünü statik olarak sun
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Anasayfa
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
